2f2cfea86070e4a0aeaaf72e47782afb02d01ad3
Elisabeth Finkel <32049677+esfinkel@users.noreply.github.com>
2018-12-27 16:56:39 -0600

    made more user-friendly. added safari support.

--- tumblr_saver.py
+++ tumblr_saver.py
@@ -1,41 +1,64 @@
-import selenium
-import os
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 
-def save(blog, num):
+def operate(blog, num, safari):
+    if safari:
+        with webdriver.Safari() as driver:
+            save(blog,num,driver)
+    else:
+        import os
+        with webdriver.Chrome(str(os.getcwd())+'/chromedriver') as driver:
+            save(blog,num,driver)
+
+def save(blog, num, driver):
     more = True
     i = 1
     if (num == 0):
         more = False
         i = i - 1
-    with webdriver.Chrome('/Applications/chromedriver') as driver:
-        while more:
-            driver.get('https://web.archive.org/save/https://'+blog+'.tumblr.com/page/'+str(i))
-            while (driver.current_url=='https://web.archive.org/save/https://'+blog+'.tumblr.com/page/'+str(i)+'/'):
-                time.sleep(1)
-            try:
-                driver.find_element_by_css_selector('#page > div.posts-no-posts.content').text
-                more = False
-                i = i - 1
-            except:
-                i = i + 1
-            if (num > 0 and i > num):
-                more = False
-                i = i - 1
-    print('saved pages with indices 1 through',i)                
+
+    while more:
+        driver.get('https://web.archive.org/save/https://'+blog+'.tumblr.com/page/'+str(i))
+        while ('web.archive.org/web/' not in driver.current_url): # when page is saved, web.archive redirects to url web.archive.org/web/something-or-other
+            time.sleep(0.2)
+        try:
+            driver.find_element_by_css_selector('#page > div.posts-no-posts.content').text
+            more = False
+            i = i - 1
+        except:
+            i = i + 1
+        if (num > 0 and i > num):
+            more = False
+            i = i - 1
+
+    print('saved pages with indices 1 through',i)        
+    
 
 if __name__ == '__main__':
     from sys import argv
-    try:
-        num = int(argv[2])
-    except:
-        num = -1
-    num = num
-    blog = argv[1]
+    
     try:
         blog = argv[1]
-        save(blog, num)
     except:
-        print('please enter your blog name (which appears in its url) after the name of this program.')
+        blog = input('What is your blog name (which appears in its url) ?\n')
+    
+    num = -1
+    for arg in argv:
+        try:
+            num = int(arg)
+        except:
+            num = num
+    if num == -1:
+        num = input('Save the first N pages. Leave blank to save them all.\nN: ')
+        try:
+            num = int(num)
+        except:
+            num = -1
+    
+    browser = 'safari' in input('Safari or Chrome?\n').lower()
+    print('Using '+browser*'Safari'+(not browser)*'Chrome')
+    
+    #try:
+    operate(blog, num, safari=browser)
+    #except:
+     #   print('program terminated prematurely.')
